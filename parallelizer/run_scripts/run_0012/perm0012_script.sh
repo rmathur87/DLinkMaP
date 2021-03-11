ebd71d1f01d312473ce8dc3dbd9a473d135a4ad4
Chinmay Rele <chinmay.rele@rutgers.edu>
2021-03-11 13:42:50 -0600

    parallelizer runs

---
+++ parallelizer/run_scripts/run_0012/perm0012_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# load R
+source /opt/asn/etc/asn-bash-profiles-special/modules.sh
+module load R/4.0.2
+
+# R CMD INSTALL ~/QTL/DLinkMap/DSPRqtl_2.0-5.tar.gz
+Rscript ~/QTL/DLinkMaP/mapping/MAP_general.R /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0012/params_0012.csv
+
