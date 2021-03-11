08d0f22bba3128573d14ef376d3e6e4a9819b95f
Chinmay Rele <26606959+drosophominin@users.noreply.github.com>
2021-03-11 09:08:43 -0600

    Create script.sh

---
+++ parallelizer/code/script.sh
@@ -0,0 +1,48 @@
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0001
+chmod +x perm0001_script.sh
+run_script perm0001_script.sh
+sleep 10
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0002
+chmod +x perm0002_script.sh
+run_script perm0002_script.sh
+sleep 10
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0003
+chmod +x perm0003_script.sh
+run_script perm0003_script.sh
+sleep 10
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0004
+chmod +x perm0004_script.sh
+run_script perm0004_script.sh
+sleep 10
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0005
+chmod +x perm0005_script.sh
+run_script perm0005_script.sh
+sleep 10
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0006
+chmod +x perm0006_script.sh
+run_script perm0006_script.sh
+sleep 10
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0007
+chmod +x perm0007_script.sh
+run_script perm0007_script.sh
+sleep 10
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0008
+chmod +x perm0008_script.sh
+run_script perm0008_script.sh
+sleep 10
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0009
+chmod +x perm0009_script.sh
+run_script perm0009_script.sh
+sleep 10
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0010
+chmod +x perm0010_script.sh
+run_script perm0010_script.sh
+sleep 10
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0011
+chmod +x perm0011_script.sh
+run_script perm0011_script.sh
+sleep 10
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0012
+chmod +x perm0012_script.sh
+run_script perm0012_script.sh
+sleep 10
