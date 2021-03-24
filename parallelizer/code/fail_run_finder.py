cd0485575c6738cadfd4176917d40f61632c6035
Chinmay Rele <chinmay.rele@rutgers.edu>
2021-03-24 13:10:06 -0500

    script to find which permutations failes, and creating a script to rerun

---
+++ parallelizer/code/fail_run_finder.py
@@ -0,0 +1,74 @@
+"""
+Identifies which runs "failed", or otherwise did not run.
+Returns a list of numbers, so that those specific runs can be run again manually.
+
+If the number of runs is too large, we may have to make it create a bash file to run (likely course of action).
+
+- If any of the file sizes are 0, it is likely that it did not run
+"""
+
+### imports ==================================================================
+from pprint import pprint as pprint
+import os
+import glob
+
+### variables ==================================================================
+
+run_directory = "/Users/rele.c/Downloads/DLinkMaP/parallelizer/run_scripts"
+rerun_script = "/Users/rele.c/Downloads/DLinkMaP/parallelizer/code/rerun.sh"
+
+### functions ==================================================================
+
+def dir_getter( path, out_type="full" ):
+	"""
+	returns the full path of directories, or only the names of the directories
+	"""
+	import os
+	directory_contents = os.listdir(path)
+
+	full_paths = [ "{0}/{1}".format( path, subdir ) for subdir in directory_contents ]
+
+	if out_type != "full":
+		return( directory_contents )
+
+	elif out_type == "full":
+		return( full_paths )
+
+### code ==================================================================""
+
+
+run_dirs = dir_getter( run_directory )
+n_runs = len( run_dirs )
+run_dirs.sort()
+
+not_run = [] # permutations that have not been run
+
+for dir in run_dirs:
+	maleWt_csv = [ "{0}/maleWt/{1}".format( dir, f ) for f in os.listdir("{0}/maleWt".format( dir )) if f.endswith('.csv') ]
+
+	total_file_size = 0
+
+	for file in maleWt_csv:
+		total_file_size += os.path.getsize( file )
+
+	if total_file_size == 0:
+		not_run.append( dir[-4:] )
+
+with open( rerun_script, "w" ) as scfile:
+	print( "# to rerun only models that have not been previously run/failed to run before", file=scfile)
+	print( "# Models not/failed to run: {0}".format( "\t".join(not_run) ), file=scfile )
+	print( file=scfile )
+	for run in not_run:
+		print( "cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_{0}".format( run ), file=scfile )
+		print( "rm perm{0}shSCRIPT.*".format( run ), file=scfile )
+		print( "rm maleWt/*.csv".format( run ), file=scfile )
+		print( "chmod +x perm{0}.sh".format( run ), file=scfile )
+		print( "run_script perm{0}.sh".format( run ), file=scfile )
+		print( "sleep 10".format( run ), file=scfile )
+		print( file=scfile )
+# for item in not_run:
+
+# cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0201
+# chmod +x perm0201.sh
+# run_script perm0201.sh
+# sleep 10
---
+++ parallelizer/code/rerun.sh
@@ -0,0 +1,409 @@
+# to rerun only models that have not been previously run/failed to run before
+# Models not/failed to run: 0015	0016	0018	0022	0025	0026	0027	0028	0029	0031	0033	0034	0035	0036	0037	0038	0039	0040	0041	0042	0043	0047	0048	0049	0052	0053	0055	0063	0064	0068	0069	0070	0071	0072	0073	0074	0075	0076	0077	0078	0079	0082	0084	0085	0086	0087	0088	0089	0090	0091	0092	0093	0094	0095	0096	0097	0098	0099
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0015
+rm perm0015shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0015.sh
+run_script perm0015.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0016
+rm perm0016shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0016.sh
+run_script perm0016.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0018
+rm perm0018shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0018.sh
+run_script perm0018.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0022
+rm perm0022shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0022.sh
+run_script perm0022.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0025
+rm perm0025shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0025.sh
+run_script perm0025.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0026
+rm perm0026shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0026.sh
+run_script perm0026.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0027
+rm perm0027shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0027.sh
+run_script perm0027.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0028
+rm perm0028shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0028.sh
+run_script perm0028.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0029
+rm perm0029shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0029.sh
+run_script perm0029.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0031
+rm perm0031shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0031.sh
+run_script perm0031.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0033
+rm perm0033shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0033.sh
+run_script perm0033.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0034
+rm perm0034shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0034.sh
+run_script perm0034.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0035
+rm perm0035shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0035.sh
+run_script perm0035.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0036
+rm perm0036shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0036.sh
+run_script perm0036.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0037
+rm perm0037shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0037.sh
+run_script perm0037.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0038
+rm perm0038shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0038.sh
+run_script perm0038.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0039
+rm perm0039shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0039.sh
+run_script perm0039.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0040
+rm perm0040shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0040.sh
+run_script perm0040.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0041
+rm perm0041shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0041.sh
+run_script perm0041.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0042
+rm perm0042shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0042.sh
+run_script perm0042.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0043
+rm perm0043shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0043.sh
+run_script perm0043.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0047
+rm perm0047shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0047.sh
+run_script perm0047.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0048
+rm perm0048shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0048.sh
+run_script perm0048.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0049
+rm perm0049shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0049.sh
+run_script perm0049.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0052
+rm perm0052shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0052.sh
+run_script perm0052.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0053
+rm perm0053shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0053.sh
+run_script perm0053.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0055
+rm perm0055shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0055.sh
+run_script perm0055.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0063
+rm perm0063shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0063.sh
+run_script perm0063.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0064
+rm perm0064shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0064.sh
+run_script perm0064.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0068
+rm perm0068shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0068.sh
+run_script perm0068.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0069
+rm perm0069shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0069.sh
+run_script perm0069.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0070
+rm perm0070shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0070.sh
+run_script perm0070.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0071
+rm perm0071shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0071.sh
+run_script perm0071.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0072
+rm perm0072shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0072.sh
+run_script perm0072.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0073
+rm perm0073shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0073.sh
+run_script perm0073.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0074
+rm perm0074shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0074.sh
+run_script perm0074.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0075
+rm perm0075shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0075.sh
+run_script perm0075.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0076
+rm perm0076shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0076.sh
+run_script perm0076.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0077
+rm perm0077shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0077.sh
+run_script perm0077.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0078
+rm perm0078shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0078.sh
+run_script perm0078.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0079
+rm perm0079shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0079.sh
+run_script perm0079.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0082
+rm perm0082shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0082.sh
+run_script perm0082.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0084
+rm perm0084shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0084.sh
+run_script perm0084.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0085
+rm perm0085shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0085.sh
+run_script perm0085.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0086
+rm perm0086shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0086.sh
+run_script perm0086.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0087
+rm perm0087shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0087.sh
+run_script perm0087.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0088
+rm perm0088shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0088.sh
+run_script perm0088.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0089
+rm perm0089shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0089.sh
+run_script perm0089.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0090
+rm perm0090shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0090.sh
+run_script perm0090.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0091
+rm perm0091shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0091.sh
+run_script perm0091.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0092
+rm perm0092shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0092.sh
+run_script perm0092.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0093
+rm perm0093shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0093.sh
+run_script perm0093.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0094
+rm perm0094shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0094.sh
+run_script perm0094.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0095
+rm perm0095shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0095.sh
+run_script perm0095.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0096
+rm perm0096shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0096.sh
+run_script perm0096.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0097
+rm perm0097shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0097.sh
+run_script perm0097.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0098
+rm perm0098shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0098.sh
+run_script perm0098.sh
+sleep 10
+
+cd /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0099
+rm perm0099shSCRIPT.*
+rm maleWt/*.csv
+chmod +x perm0099.sh
+run_script perm0099.sh
+sleep 10
+
