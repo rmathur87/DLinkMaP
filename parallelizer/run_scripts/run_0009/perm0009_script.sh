cda4b39fe356692bd8581d6c5bed8c4f8ec64fab
Chinmay Rele <chinmay.rele@rutgers.edu>
2021-03-24 11:31:59 -0500

    added run files for runs [1-200] to parse which ones ran/did not run

--- parallelizer/run_scripts/run_0009/perm0009_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# load R
-source /opt/asn/etc/asn-bash-profiles-special/modules.sh
-module load R/4.0.2
-
-# R CMD INSTALL ~/QTL/DLinkMap/DSPRqtl_2.0-5.tar.gz
-Rscript ~/QTL/DLinkMaP/mapping/MAP_general.R /home/ualcpr/QTL/DLinkMaP/parallelizer/run_scripts/run_0009/params_0009.csv
-
