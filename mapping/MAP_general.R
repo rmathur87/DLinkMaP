4810cd031472fc63ab3d3cfae40431f844541b8f
Chinmay Rele <chinmay.rele@rutgers.edu>
2021-04-01 10:41:22 -0500

    print statement edit

--- mapping/MAP_general.R
+++ mapping/MAP_general.R
@@ -136,7 +136,7 @@ system.time({
   for(i in 1:nrow(poslist)) {
     #for(i in 1:10) {
     if ((i %% 10) == 0) {
-      print(paste( str_pad(i, 7, 'left', ' '), nrow(poslist), sep=" of " ))
+      print( paste( i, " / ", nrow(poslist), sep = " ", collapse = NULL) )
     }
     #Input:
     #i is the position in the genome
