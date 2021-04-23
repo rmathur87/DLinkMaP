6978452c5e5eefb233985ffc1da2976d67eab75c
Chinmay Rele <chinmay.rele@rutgers.edu>
2021-04-23 14:24:53 -0500

    edited for params run

--- parallelizer/code/tg-parallelizer.py
+++ parallelizer/code/tg-parallelizer.py
@@ -197,7 +197,7 @@ for i in range( n_runs ):
 			if line.split(",")[0] == "phenotype":
 				temp_line = [
 					line.split(",")[0],
-					line.split(",")[1],
+					",".join(line.split(",")[1:-2]),
 					"TG"
 				]
 				add_line = ",".join(temp_line)
